c2377bd97479bfa1cd4354588cd3bf834fc3d52f
Papibadiste <labussieremailpro@gmail.com>
2020-09-01 14:58:25 +0200

    commit de mise en place du test

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
 <?php
 
-//Etape-1: 
-//Voici un array de 8 valeur, pour chaque valeur et avec une fonction revoyer:
-//LETS si la valeur est divisible uniquement par 3
-//EAT si la valeur est divisible uniquement par 5
-//LETSEAT si la valeur est divisible par 3 et 5
-//La valeur initial si la valeur n'est divisible par 3 et 5
-echo "<h2> Etape1 </h2>";
-
-$array = array("6", "10", "15", "38","30000", "19685", "6022", "6756");
-
-
-
-
-
-///Etape-2: Pour la chaine de caractere suivantes
-
-echo "<h2> Etape2 </h2>";
-
-$S = "ABCDEF123456789";
-
-
-
-
-
-///Etape-3:
-// Pour la chaine de caractere suivantes verifier si il y a plus de 8 caractere 
-// Si il y a une majuscule, une minuscule et un chiffre
-// Retourner une erreur
-
-echo "<h2> Etape3 </h2>";
-
-$S= "kA9kzejh2";
-
-
-echo $result;
+echo "oui";
